caadbe983f8117ab8234ef1dd4492c13e7fb0e8a
DaniDeltaT <DaniDelta82@gmail.com>
2025-08-11 11:33:03 -0500

    change healthcheck to POST (#34)
    * change healthcheck to POST

--- plugins/pkg/plugin/beacon/health.go
+++ plugins/pkg/plugin/beacon/health.go
@@ -10,15 +10,22 @@ import (
 )
 
 func IpfsHealthCheck(ctx context.Context) (health.HealthState, error) {
-	cfg := readFromEnv()
+	cfg := readFromEnv() // should resolve to IPFSAddress used by ipfs plugin
 	client := &http.Client{Timeout: 2 * time.Second}
-	resp, err := client.Get(cfg.IPFSAddress + "/api/v0/version")
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, cfg.IPFSAddress+"/api/v0/version", nil)
+	if err != nil {
+		return health.HealthStateUnhealthy, fmt.Errorf("build request: %w", err)
+	}
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return health.HealthStateUnhealthy, fmt.Errorf("IPFS not reachable: %w", err)
 	}
 	defer func() {
 		_ = resp.Body.Close()
 	}()
+
 	if resp.StatusCode != http.StatusOK {
 		return health.HealthStateUnhealthy, fmt.Errorf("IPFS API error: %s", resp.Status)
 	}
