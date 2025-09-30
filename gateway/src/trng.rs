b8bfe32570d1c5286052a733fd6348c8baf7716f
DaniDeltaT <danidelta82@gmail.com>
2025-09-30 13:20:34 -0500

    METRIC collections for fallback moved innto fallback func

--- gateway/src/trng.rs
+++ gateway/src/trng.rs
@@ -151,12 +151,20 @@ impl TrngService {
     }
 
     async fn fallback(&mut self) -> Result<TrngResponse, GatewayError> {
+        metrics::TRNG_FALLBACKS_COUNTER
+        .with_label_values(&["derived"])
+        .inc();
+
         if let Some(master_seed) = self.get_next_master_seed() {
             let mut rng = rand::rng();
             let index = rng.random_range(0..ChildNumber::HARDENED_FLAG);
             match master_seed.derive(index) {
                 Ok(key) => {
                     let trng = to_trng(key);
+                    metrics::TRNG_FALLBACKS_COUNTER
+                    .with_label_values(&["ok"])
+                    .inc();
+
                     let resp = TrngResponse {
                         values: vec![trng],
                         sig: "".to_string(),
@@ -164,6 +172,10 @@ impl TrngService {
                     Ok(resp)
                 }
                 Err(e) => {
+                    metrics::TRNG_FALLBACKS_COUNTER
+                    .with_label_values(&["err"])
+                    .inc();
+
                     tracing::error!("Failed to derive key from master seed: {}", e);
                     Err(GatewayError::InternalError(
                         "Failed to derive key".to_string(),
@@ -171,6 +183,10 @@ impl TrngService {
                 }
             }
         } else {
+            metrics::TRNG_FALLBACKS_COUNTER
+            .with_label_values(&["err"])
+            .inc();
+
             tracing::error!("No master seed available");
             Err(GatewayError::InternalError(
                 "No master seed available".to_string(),
@@ -194,13 +210,7 @@ impl ServiceHandler for TrngService {
                 if trng.values.is_empty() {
                     tracing::warn!("TRNG returned empty values; attempting fallback");
                     if svc_req.src.contains(&SRC_DERIVED_TRNG.to_string()) {
-                        metrics::TRNG_FALLBACKS_COUNTER
-                            .with_label_values(&["derived"])
-                            .inc();
                         trng = self.fallback().await?;
-                        metrics::TRNG_FALLBACKS_COUNTER
-                            .with_label_values(&["ok"])
-                            .inc();
                     } else {
                         return Err(GatewayError::InternalError("no values in TRNG".to_string()));
                     }
@@ -228,13 +238,7 @@ impl ServiceHandler for TrngService {
                 tracing::warn!("Failed to get trng from aptos orbital: {}", e);
                 if svc_req.src.contains(&SRC_DERIVED_TRNG.to_string()) {
                     tracing::info!("Fallback to derived trng");
-                    metrics::TRNG_FALLBACKS_COUNTER
-                        .with_label_values(&["derived"])
-                        .inc();
                     let trng = self.fallback().await?;
-                    metrics::TRNG_FALLBACKS_COUNTER
-                        .with_label_values(&["ok"])
-                        .inc();
                     let bulk_results = if svc_req.bulk.is_some() {
                         let first_val = trng.values.first().cloned().ok_or_else(|| {
                             GatewayError::InternalError("no values in TRNG".to_string())
