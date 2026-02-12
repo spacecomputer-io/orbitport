a0b97b1cbd26f6573ee5d05d3e432e92a7a4df98
amirylm <83904651+amirylm@users.noreply.github.com>
2026-02-12 20:00:53 +0200

    update url

--- gateway/src/health.rs
+++ gateway/src/health.rs
@@ -3,9 +3,8 @@ use warp::Filter;
 use tracing::{debug, info};
 
 /// Starts a health check server on the specified port.
-/// Using warp to create a simple HTTP server that responds to /health with 200 OK and payload {"status": "ok"}.
 pub async fn start_health_check_server(port: u16) {
-    let health_route = warp::path("health").map(|| {
+    let health_route = warp::path("healthz").map(|| {
         debug!("Received health check request");
         warp::reply::json(&serde_json::json!({
             "status": "ok"
