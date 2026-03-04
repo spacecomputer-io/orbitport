d299f51340461e6f6551baba48f1e1602c9f92ba
Nicolás Rivero Corvalán <riveronicolas@gmail.com>
2026-03-04 16:58:39 -0300

    infra-277 (#86)
    * infra-277
    
    * infra-277
    
    * infra-277

--- plugins/pkg/plugin/auth/config.go
+++ plugins/pkg/plugin/auth/config.go
@@ -10,6 +10,7 @@ type authConfig struct {
 	Auth0Domain string
 	// Auth0Audience is the audience of the Auth0 instance.
 	Auth0Audience string
+	DisableAuth   bool
 }
 
 func readFromEnv() *authConfig {
@@ -18,10 +19,12 @@ func readFromEnv() *authConfig {
 	return &authConfig{
 		Auth0Domain:   viper.GetString("AUTH0_DOMAIN"),
 		Auth0Audience: viper.GetString("AUTH0_AUDIENCE"),
+		DisableAuth:   viper.GetBool("DEV_DISABLE_AUTH"),
 	}
 }
 
 func setDefaults() {
 	viper.SetDefault("AUTH0_DOMAIN", "")
 	viper.SetDefault("AUTH0_AUDIENCE", "")
+	viper.SetDefault("DEV_DISABLE_AUTH", false) // defaults to secure
 }
