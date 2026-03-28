706e71386477b68edeb0c78d6f239bcc7b9fea83
DaniDeltaT <DaniDelta82@gmail.com>
2026-03-28 15:58:29 -0500

    refactor match rejection of unexpected values

--- gateway/src/types.rs
+++ gateway/src/types.rs
@@ -85,9 +85,7 @@ impl EncryptionKey {
         let scheme = match parts[0] {
             "" => EncryptionScheme::None,
             "threshold" => EncryptionScheme::Threshold,
-            _ => {
-                return Err(GatewayError::InvalidEncryptionScheme(parts[0].to_string()));
-            }
+            x => return Err(GatewayError::InvalidEncryptionScheme(x.to_string())),
         };
         Ok(EncryptionKey { key, scheme })
     }
