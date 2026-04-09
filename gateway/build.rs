ee7ff08036458f518ac05605de049c3e8e790184
amirylm <83904651+amirylm@users.noreply.github.com>
2026-04-09 20:23:13 +0300

    added new proto folder and update deps

--- gateway/build.rs
+++ gateway/build.rs
@@ -1,19 +1,34 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    // compiling protobuf files
-    tonic_build::configure()
+    // compiling plugin protos
+    tonic_prost_build::configure()
         .build_server(false)
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
             &[
-                "proto/auth.proto",
-                "proto/trng.proto",
-                "proto/ipfs.proto",
-                "proto/masterseed.proto",
+                "auth.proto",
+                "trng.proto",
+                "ipfs.proto",
+                "masterseed.proto",
             ],
-            &["proto"], // specify the root location to search proto dependencies
+            &["../proto/plugins"], // specify the root location to search proto dependencies
         )
         .unwrap();
+
+    // compiling services protos
+    tonic_prost_build::configure()
+        .build_server(false)
+        .protoc_arg("--experimental_allow_proto3_optional")
+        // Add serde attributes to all generated message structs
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .compile_protos(
+            &[
+                "ctrng.proto",
+            ],
+            &["../proto/services"], // specify the root location to search proto dependencies
+        )
+        .unwrap();
+
     Ok(())
 }
