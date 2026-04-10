d2a777aa038b5ee6cbb765361e9be8a3dda6ef9f
amirylm <83904651+amirylm@users.noreply.github.com>
2026-04-10 19:23:00 +0300

    build docker fix (wip)
    this is a temp workaround, it forces to build with makefile,
    otherwise the proto folder will be missing

--- gateway/build.rs
+++ gateway/build.rs
@@ -1,17 +1,16 @@
 use std::io::Result;
 
-fn main() -> Result<()> {
-    // compiling plugin protos
+fn build_plugins(proto_dir: &str) -> std::io::Result<()> {
     tonic_prost_build::configure()
         .build_server(false)
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
             &["auth.proto", "ao.proto", "ipfs.proto", "masterseed.proto"],
-            &["../proto/plugins"], // specify the root location to search proto dependencies
+            &[proto_dir], // specify the root location to search proto dependencies
         )
-        .unwrap();
+}
 
-    // compiling services protos
+fn build_services(proto_dir: &str) -> std::io::Result<()> {
     tonic_prost_build::configure()
         .build_server(false)
         .protoc_arg("--experimental_allow_proto3_optional")
@@ -19,9 +18,20 @@ fn main() -> Result<()> {
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .compile_protos(
             &["ctrng.proto"],
-            &["../proto/services"], // specify the root location to search proto dependencies
+            &[proto_dir], // specify the root location to search proto dependencies
         )
-        .unwrap();
+}
+
+fn main() -> Result<()> {
+    if let Err(_e) = build_plugins("../proto/plugins") {
+        // trying from current directory if the first path doesn't work
+        build_plugins("./proto/plugins").unwrap();
+    }
+
+    if let Err(_e) = build_services("../proto/services") {
+        // trying from current directory if the first path doesn't work
+        build_services("./proto/services").unwrap();
+    }
 
     Ok(())
 }
