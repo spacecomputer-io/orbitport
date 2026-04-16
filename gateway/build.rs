3f956b6598a6fcf046fdc89ec66644e92cf6b4ca
amirylm <83904651+amirylm@users.noreply.github.com>
2026-04-16 20:24:41 +0300

    load proto files dynamically during build to avoid hard coded values

--- gateway/build.rs
+++ gateway/build.rs
@@ -1,37 +1,50 @@
 use std::io::Result;
 
-fn build_plugins(proto_dir: &str) -> std::io::Result<()> {
+fn main() -> Result<()> {
+    let (plugin_dir, plugin_protos) = protos_in_dir("../proto/plugins").or_else(|_| protos_in_dir("./proto/plugins"))?;
+
     tonic_prost_build::configure()
         .build_server(false)
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
-            &["auth.proto", "ao.proto", "ipfs.proto", "masterseed.proto"],
-            &[proto_dir], // specify the root location to search proto dependencies
-        )
-}
+            &plugin_protos.iter().map(|s| s.as_str()).collect::<Vec<_>>(),
+            &[&plugin_dir],
+        )?;
 
-fn build_services(proto_dir: &str) -> std::io::Result<()> {
+    let (service_dir, service_protos) = protos_in_dir("../proto/services").or_else(|_| protos_in_dir("./proto/services"))?;
     tonic_prost_build::configure()
         .build_server(false)
         .protoc_arg("--experimental_allow_proto3_optional")
         // Add serde attributes to all generated message structs
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .compile_protos(
-            &["ctrng.proto"],
-            &[proto_dir], // specify the root location to search proto dependencies
-        )
-}
-
-fn main() -> Result<()> {
-    if let Err(_e) = build_plugins("../proto/plugins") {
-        // trying from current directory if the first path doesn't work
-        build_plugins("./proto/plugins").unwrap();
-    }
-
-    if let Err(_e) = build_services("../proto/services") {
-        // trying from current directory if the first path doesn't work
-        build_services("./proto/services").unwrap();
-    }
+            &service_protos.iter().map(|s| s.as_str()).collect::<Vec<_>>(),
+            &[&service_dir],
+        )?;
 
     Ok(())
+}
+
+fn protos_in_dir(dir: &str) -> Result<(String, Vec<String>)> {
+    // if not exist return error
+    if !std::path::Path::new(dir).exists() {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            format!("Directory {} not found", dir),
+        ));
+    }
+    let mut protos = Vec::new();
+    for entry in std::fs::read_dir(dir)? {
+        let entry = entry?;
+        if entry.file_type()?.is_dir() {
+            let (_, mut nested_protos) = protos_in_dir(&entry.path().to_str().unwrap())?;
+            protos.append(&mut nested_protos);
+            continue;
+        }
+        let path = entry.path();
+        if path.extension().and_then(|s| s.to_str()) == Some("proto") {
+            protos.push(path.to_str().unwrap().to_string());
+        }
+    }
+    Ok((dir.to_string(), protos))
 }
